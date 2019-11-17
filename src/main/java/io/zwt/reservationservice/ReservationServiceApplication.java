d2f20416db834a946f767fae46b1c29b72f581c4
招文桃 <gdouzwt@gmail.com>
2019-11-17 13:44:23 +0800

    事务代码调试通过

--- src/main/java/io/zwt/reservationservice/ReservationServiceApplication.java
+++ src/main/java/io/zwt/reservationservice/ReservationServiceApplication.java
@@ -1,5 +1,6 @@
 package io.zwt.reservationservice;
 
+import io.r2dbc.spi.ConnectionFactory;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,21 +9,61 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.event.SpringApplicationEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.r2dbc.connectionfactory.R2dbcTransactionManager;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.ReactiveTransactionManager;
+import org.springframework.transaction.reactive.TransactionalOperator;
+import org.springframework.util.Assert;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+
+import java.nio.channels.FileLock;
 
 @SpringBootApplication
 public class ReservationServiceApplication {
+
+    @Bean
+    ReactiveTransactionManager reactiveTransactionManager(ConnectionFactory cf) {
+        return new R2dbcTransactionManager(cf);
+    }
+
+    @Bean
+    TransactionalOperator transactionalOperator(ReactiveTransactionManager transactionManager) {
+        return TransactionalOperator.create(transactionManager);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(ReservationServiceApplication.class, args);
     }
 
+}
+
+@Service
+@RequiredArgsConstructor
+class ReservationService {
+
+    private final ReservationRepository reservationRepository;
+    private final TransactionalOperator transactionalOperator;
+
+    Flux<Reservation> saveAll(String... names) {
+        return this.transactionalOperator
+                .transactional(
+                Flux
+        .fromArray(names)
+        .map(name -> new Reservation(null, name))
+        .flatMap(this.reservationRepository::save)
+        .doOnNext(r -> Assert.isTrue(isValid(r), "The name must have a capital first letter!")));
+    }
+
+    private boolean isValid(Reservation r) {
+        return Character.isUpperCase(r.getName().charAt(0));
+    }
 }
 
 @Component
@@ -31,15 +72,15 @@ public class ReservationServiceApplication {
 class SampleDataInitializer {
 
     private final ReservationRepository reservationRepository;
+    private final ReservationService reservationService;
 
     @EventListener(ApplicationReadyEvent.class)
     public void ready() {
 
-        var saved = Flux.just("Josh", "Madhura", "Mark", "Olga", "Spencer", "Ria", "Stéphane", "Violetta")
-                .map(name -> new Reservation(null, name))
-                .flatMap(this.reservationRepository::save);
+        var saved = this.reservationService.saveAll("Josh", "Madhura", "Mark", "Olga", "Spencer", "Ria", "Stéphane", "Violetta");
 
-        this.reservationRepository.deleteAll()
+        this.reservationRepository
+                .deleteAll()
                 .thenMany(saved)
                 .thenMany(this.reservationRepository.findAll())
                 .subscribe(log::info);
@@ -47,17 +88,16 @@ class SampleDataInitializer {
 
 }
 
-interface ReservationRepository extends ReactiveCrudRepository<Reservation, String> {
+interface ReservationRepository extends ReactiveCrudRepository<Reservation, Integer> {
 
 }
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document
 class Reservation {
 
     @Id
-    private String id;
+    private Integer id;
     private String name;
 }
